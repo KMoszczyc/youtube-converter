87fbd20777845d9a86e2fd6426286649b3eb7863
Kamil Moszczyc <likeyouop@gmail.com>
2022-10-26 12:42:29 +0200

    Switch to render.com

--- public/script.js
+++ public/script.js
@@ -1,9 +1,9 @@
 var browser = browser || chrome;
 
 //  host
-// const server_endpoint = "https://youtube-converter.onrender.com"
+const server_endpoint = "https://youtube-converter.onrender.com"
 // const server_endpoint = "https://ytmp3-converter.herokuapp.com"
-const server_endpoint = "https://cloudy-pig-trench-coat.cyclic.app";
+// const server_endpoint = "https://cloudy-pig-trench-coat.cyclic.app";
 // const server_endpoint = "http://localhost:3000";
 
 // endpoints
